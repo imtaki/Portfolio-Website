d217c65b1870084806ca5356d6d8fc561ea7f30b
imtaki <161586687+imtaki@users.noreply.github.com>
2025-01-17 14:30:27 +0100

    Updated TechStack
    Added NextJS, TypeScript to my tech stack list

--- src/components/About/TechStack/TechStack.jsx
+++ src/components/About/TechStack/TechStack.jsx
@@ -1,10 +1,12 @@
 import { FaReact, FaVuejs, FaGitAlt, FaNode } from "react-icons/fa";
-import { SiTailwindcss, SiSass, SiMongodb, SiJavascript, SiExpress, SiPython, SiSqlite } from "react-icons/si"; 
+import { RiNextjsFill } from "react-icons/ri";
+import { SiTailwindcss, SiSass, SiMongodb, SiJavascript, SiExpress, SiPython, SiSqlite, SiTypescript } from "react-icons/si"; 
 import './index.scss'
 
 export default function TechStack() {
   const techIcons = {
     ReactJS: <FaReact color="#61dafb" />,
+    NextJS: <RiNextjsFill color="#181818"/>,
     VueJS: <FaVuejs color="#4FC08D" />,
     ExpressJS: <SiExpress color="#000000" />,
     TailwindCSS: <SiTailwindcss color="#38BDF8" />,
@@ -12,6 +14,7 @@ export default function TechStack() {
     Python: <SiPython color="#0576f7" />,
     MongoDB: <SiMongodb color="#47A248" />,
     JavaScript: <SiJavascript color="#F7DF1E" />,
+    TypeScript: <SiTypescript color="#3178C6" />,
     SQL: <SiSqlite color="#003B57" />,
     Git: <FaGitAlt color="#fc5908"/>,
     NodeJS: <FaNode color="green"/>
@@ -21,7 +24,7 @@ export default function TechStack() {
     <div className="tech-stack">
       <h2>Tech Stack</h2>
       <ul className="tech-list">
-        {["ReactJS", "VueJS", "ExpressJS", "TailwindCSS", "Sass", "Python", "MongoDB", "JavaScript", "SQL", "Git", "NodeJS"].map((tech, index) => (
+        {["ReactJS", "NextJS", "VueJS", "ExpressJS", "TailwindCSS", "Sass", "Python", "MongoDB", "JavaScript", "TypeScript", "SQL", "Git", "NodeJS"].map((tech, index) => (
           <li key={index} className="tech-item">
             <div className="tech-icon">{techIcons[tech]}</div>
             <p>{tech}</p>
