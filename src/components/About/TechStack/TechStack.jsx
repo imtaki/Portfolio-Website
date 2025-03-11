77db10f271fc531f08cad22004ed5d23e3aacded
imtaki <161586687+imtaki@users.noreply.github.com>
2025-03-11 10:39:08 +0100

    Small syntax/ui changes

--- src/components/About/TechStack/TechStack.jsx
+++ src/components/About/TechStack/TechStack.jsx
@@ -1,36 +1,38 @@
 import { FaReact, FaVuejs, FaGitAlt, FaNode } from "react-icons/fa";
 import { RiNextjsFill } from "react-icons/ri";
-import { SiTailwindcss, SiSass, SiMongodb, SiJavascript, SiExpress, SiPython, SiSqlite, SiTypescript } from "react-icons/si"; 
-import './index.scss'
+import { SiTailwindcss, SiSass, SiMongodb, SiJavascript, SiExpress, SiPython, SiSqlite, SiTypescript } from "react-icons/si";
+import './index.scss';
 
-export default function TechStack() {
-  const techIcons = {
-    ReactJS: <FaReact color="#61dafb" />,
-    NextJS: <RiNextjsFill color="#181818"/>,
-    VueJS: <FaVuejs color="#4FC08D" />,
-    ExpressJS: <SiExpress color="#000000" />,
-    TailwindCSS: <SiTailwindcss color="#38BDF8" />,
-    Sass: <SiSass color="#CD6799" />,
-    Python: <SiPython color="#0576f7" />,
-    MongoDB: <SiMongodb color="#47A248" />,
-    JavaScript: <SiJavascript color="#F7DF1E" />,
-    TypeScript: <SiTypescript color="#3178C6" />,
-    SQL: <SiSqlite color="#003B57" />,
-    Git: <FaGitAlt color="#fc5908"/>,
-    NodeJS: <FaNode color="green"/>
-  };
+const TechStack = () => {
+  const technologies = [
+    { name: "ReactJS", icon: <FaReact color="#61dafb" /> },
+    { name: "NextJS", icon: <RiNextjsFill color="#181818" /> },
+    { name: "VueJS", icon: <FaVuejs color="#4FC08D" /> },
+    { name: "ExpressJS", icon: <SiExpress color="#000000" /> },
+    { name: "TailwindCSS", icon: <SiTailwindcss color="#38BDF8" /> },
+    { name: "Sass", icon: <SiSass color="#CD6799" /> },
+    { name: "Python", icon: <SiPython color="#0576f7" /> },
+    { name: "MongoDB", icon: <SiMongodb color="#47A248" /> },
+    { name: "JavaScript", icon: <SiJavascript color="#F7DF1E" /> },
+    { name: "TypeScript", icon: <SiTypescript color="#3178C6" /> },
+    { name: "SQL", icon: <SiSqlite color="#003B57" /> },
+    { name: "Git", icon: <FaGitAlt color="#fc5908" /> },
+    { name: "NodeJS", icon: <FaNode color="green" /> }
+  ];
 
   return (
     <div className="tech-stack">
       <h2>Tech Stack</h2>
       <ul className="tech-list">
-        {["ReactJS", "NextJS", "VueJS", "ExpressJS", "TailwindCSS", "Sass", "Python", "MongoDB", "JavaScript", "TypeScript", "SQL", "Git", "NodeJS"].map((tech, index) => (
+        {technologies.map((tech, index) => (
           <li key={index} className="tech-item">
-            <div className="tech-icon">{techIcons[tech]}</div>
-            <p>{tech}</p>
+            <div className="tech-icon">{tech.icon}</div>
+            <p>{tech.name}</p>
           </li>
         ))}
       </ul>
     </div>
   );
-}
+};
+
+export default TechStack;
